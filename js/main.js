3e682b057c0306865ee7684c7502426febf4c429
TolikBu <tolikelement@gmail.com>
2021-11-02 18:21:39 +0500

    add block future

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 
-function moveWin() {
-  window.scroll(0, 10000);
-  setTimeout("moveWin();", 1000);
-}
-moveWin();
+const swiperFeature = new Swiper(".feature-slider__swiper-container", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-buttons__prev",
+    prevEl: ".slider-buttons__next",
+  },
+});
+
+
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 10000000);
+};
+
 
