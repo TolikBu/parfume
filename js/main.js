dcf6d95251e87cd1b8a2e72194bcadf6ac19e4d6
TolikBu <tolikelement@gmail.com>
2021-11-04 22:21:31 +0500

    add service block

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-
-const swiperFeature = new Swiper(".feature-slider__swiper-container", {
-  // Optional parameters
+const featureSlider = new Swiper(".feature-slider__swiper-container", {
   loop: true,
   slidesPerView: 3,
 
-  // Navigation arrows
   navigation: {
     nextEl: ".slider-buttons__prev",
     prevEl: ".slider-buttons__next",
   },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
 
+const serviceSlider = new Swiper(".service-slider__container", {
+  // loop: true,
+  slidesPerView: 2,
+  spaceBetween: 35,
 
+  navigation: {
+    nextEl: ".service-slider__button--prev",
+    prevEl: ".service-slider__button--next",
+    disabledClass: "service-slider__button-disabled",
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 10000000);
 };
-
-
